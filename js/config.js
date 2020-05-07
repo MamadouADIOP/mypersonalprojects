9bd45602da5ec4eb8db1218fa74674bf9ae892a6
mdiop <diop20@hotmail.com>
2020-05-07 01:26:00 +0000

    Tutorial of AWS APIGateway, Lambda, Cognito, IAM and DynamodB

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_d67lz1MZ9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4skfe0glhfukffacet277709n5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://ok9mom1k75.execute-api.ca-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
